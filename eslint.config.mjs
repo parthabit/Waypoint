5257bd18c104f52bf625eea9c25687e561f12597
Partha Das <pd6434938@gmail.com>
2026-09-05 20:55:56 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
+ 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
